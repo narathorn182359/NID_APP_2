ce09e23cbedc1f19194d4aa3e2200dee04ad519c
narathorn182359 <narathorn2539@hotmail.com>
2020-06-09 22:17:07 +0700

    Update Laravel.ts

--- src/settings/Laravel.ts
+++ src/settings/Laravel.ts
@@ -6,8 +6,8 @@ export const Service: any = {
     //url: 'http://18.140.109.247/nidapi',
     //apiUrl: 'http://18.140.109.247/nidapi/api',
  
-    url: 'http://127.0.0.1:8000',
-    apiUrl: 'http://127.0.0.1:8000/api',
+    url: 'http://localhost:8100',
+    apiUrl: 'http://localhost:8100/api',
 
       /**
      *
@@ -15,8 +15,8 @@ export const Service: any = {
      */
     passport: {
         'grant_type': 'password',
-        'client_id': '1',
-        'client_secret': '6Op5WO81R1x7ggTTTcUddkKukXEBgPaqXEQYm3EP',
+        'client_id': '3',
+        'client_secret': 'P4cJDTMGzaAhcuCuKSSCYbw2OZkluchgdjlDhPZt',
     }
 
 };
