d68a771302fcbdd331c8e3e5c6d323222aa89601
narathorn182359 <narathorn2539@hotmail.com>
2020-08-10 10:43:48 +0700

    Update Laravel.ts

--- src/settings/Laravel.ts
+++ src/settings/Laravel.ts
@@ -3,13 +3,13 @@ export const Service: any = {
     /**
      * Url of your Laravel Project
      */
- url: 'https://111loves.com',
-  apiUrl: 'https://111loves.com/api',
+// url: 'https://111loves.com',
+ // apiUrl: 'https://111loves.com/api',
 /*   url: 'https://111loves.com/nid_deverlop/public',
   apiUrl: 'https://111loves.com/nid_deverlop/public/api', */
  
-   //url: 'http://127.0.0.1:8000',
-   //apiUrl: 'http://127.0.0.1:8000/api',
+   url: 'http://127.0.0.1:8000',
+   apiUrl: 'http://127.0.0.1:8000/api',
 
 
       /**
